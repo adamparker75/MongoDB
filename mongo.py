1785877bb9a65178d4ec38082ded0f4d185bf8bd
adamparker75 <adamparker75@sky.com>
2020-10-11 09:37:18 +0000

    initial commit

---
+++ mongo.py
@@ -0,0 +1,35 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")  # Gets the URI from the env.py file
+DATABASE = "MyTestDB"
+COLLECTION = "MyFirstMDB"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+"""
+To update a record use the update_one(method), we pass in the key that we want to change
+and use $set keyword, which takes a key value pair in another dictionary
+"""
+
+coll.update_many({"nationality": "american"}, {"$set": {"hair_colour": "maroon"}})
+
+documents = coll.find({"nationality": "american"})
+
+for doc in documents:
+    print(doc)
